2dabad49751ac6e37ef17491ee53c4cc7c6091f7
zhuchentong <2037630@163.com>
2017-08-15 17:53:15 +0800

    udpate

---
+++ imports/app.js
@@ -0,0 +1,19 @@
+
+// Libs
+import {Meteor} from 'meteor/meteor';
+import {Router} from 'meteor/akryum:vue-router';
+
+// Create router instance
+const router = new Router({
+  history: true,
+  saveScrollPosition: true
+});
+
+// App layout
+import AppLayout from '/imports/ui/AppLayout.vue';
+
+// App start
+Meteor.startup(() => {
+  // Start the router
+  router.start(AppLayout, 'app');
+});
